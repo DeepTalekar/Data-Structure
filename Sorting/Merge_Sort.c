ca4095ea32a1d399552b80f4ce47684faac86c09
Deep <deeputalekar@gmail.com>
2020-10-06 23:44:47 +0530

    Added Merge Sort

--- Sorting/Merge_Sort.c
+++ Sorting/Merge_Sort.c
@@ -20,6 +20,12 @@ void main()
     printf("So let me know the length of the first array:\n");
     scanf("%d", &end1);
 
+    while(end1 > length)
+    {
+        printf("Please enter a valid range!! You decided to create an array out of bound!!\nPlease enter an integer within %d:\n", length);
+        scanf("%d", &end1);
+    }
+
     end2 = length - end1;
     printf("Hence the length of the another array will be %d\n", end2);
 
@@ -38,16 +44,56 @@ void main()
     printf("So you have entered the First array as follows:\n");
     print(a, end1);
 
-    printf("And now the whole array entered by you is as follows:\n");
+    printf("And now the whole array entered by you is as follows {UNSORTED ARRAY} :\n");
     print(a, length);
-    
+    printf("The length of the full array is: %d\n", length);
+
     mergeSort(a, 0, length);
+    
+    // SORTED ARRAY
+    print(a, length);
 
 }
 
 void mergeArray(int a[], int start1, int end1, int start2, int end2)
 {
     int i, j, k;
+    int *temp = NULL;
+    temp = (int *) calloc ((end1+end2), sizeof(int));
+    i = start1;
+    j = start2;
+    k = 0;
+
+    while (i <= end1 && j <= end2)
+    {
+        if(a[i] < a[j])
+            temp[k++] = a[i++];
+        else
+            temp[k++] = a[j++];
+    }
+
+    while( i <= end1 )
+        temp[k++] = a[i++];
+
+    while( j <= end2 )
+        temp[k++] = a[j++];
+    
+    for(i = start1, j = 0; i <= end2; i++, j++)
+        a[i] = temp[j];
+}
+
+void mergeSort(int a[], int start, int end)
+{
+    int middle;
+    if(start < end)
+    {
+        middle = (start + end) / 2;
+
+        mergeSort(a, start, middle);
+        mergeSort(a, middle + 1, end);
+        mergeArray(a, start, middle, middle+1, end);
+    }
+        
 }
 
 void print(int a[], int n)
